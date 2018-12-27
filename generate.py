1b2caf328bd3c38852ba242e1cddcbf081157ba1
Måns Magnusson <exoji2e@gmail.com>
2018-12-27 17:09:55 +0100

    now reads a csv and generates a pdf for each line

--- generate.py
+++ generate.py
@@ -1,5 +1,14 @@
 import subprocess
 import os
+import argparse
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--csv')
+    parser.add_argument('-o', '--outdir', default='out')
+    parser.add_argument('-i', '--startindex', type=int, default=1) 
+    return parser.parse_args()
+
+
 def produce_pdf(d, out):
     content = open('template.tex', 'r').read()
     for k, v in d.items():
@@ -13,15 +22,37 @@ def produce_pdf(d, out):
         print('Error while generating {}'.format(out))
         return
     os.rename(tmpprefix + '.pdf', out)
+    suffx = ['.aux', '.tex', '.log']
+    for v in suffx:
+        os.unlink(tmpprefix + v)
 
-d = {'INVOICENUMBER': '0', 
-	'INVOICEDATE': "2018-12-26", 
-	"YOURREF": "person ett",
-	"OURREF": "person två",
-	"ADDRESS": "Abcgatan 123",
-	"PRODUCTS": r"\product{Programmeringsläger elev}{1500.0}{3}",
- }
+def get_inject(i, row):
+    d = {"INVOICENUMBER": str(i), 
+    "INVOICEDATE": "2018-12-26", 
+    "LASTDATE" : "2019-01-25",
+    "UVSREF": "Lars Åström",
+    }
+    d["YOURREF"] = row["Referens"]
+    d["ADDRESS"] = '\\\\\n'.join(row["Adress"].split('\n'))
+    d["PRODUCTS"] = get_products(row["Anmälda Elever"], row["Anmälda Lärare"])
+    return d
+
+def get_products(elever, lärare):
+    s = []
+    if int(elever):
+        s.append(r"\product{Programmeringsläger elev}{1500.0}{" + str(elever) + "}")
+    if int(lärare):
+        s.append(r"\product{Programmeringsläger lärare}{3000.0}{" + str(lärare) + "}")
+    return "\\\\\n".join(s)
 
 
-produce_pdf(d, 'herp.pdf')
-
+if __name__ == '__main__':
+    import csv
+    args = get_args()
+    with open('anmalningar.csv') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for i, row in enumerate(reader):
+            fno = i + args.startindex
+            inject_dict = get_inject(fno, row)
+            print(inject_dict)
+            produce_pdf(inject_dict, '{}.pdf'.format(fno))
